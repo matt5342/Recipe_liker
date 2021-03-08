3aad22097bc0d1a7271a28f5d8e635981a565ce9
Matt <mattfsewell@gmail.com>
2021-03-08 12:56:18 -0500

    bootstrap added

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,45 @@
-document.addEventListener("DOMContentLoaded", () => {
-    getRecipe();
+
+// document.addEventListener("DOMContentLoaded", () => {
+//     getRecipe();
+// })
+document.getElementById("home-button").addEventListener("click", () => {
+    renderHome();
 })
-function getRecipe() {
-    fetch('http://localhost:3000/recipes/')
-    .then(res => res.json())
-    .then(recipes => recipes.forEach(renderRecipe))
-}
-function renderRecipe(e) {
+document.getElementById("cooking-styles").addEventListener("click", () => {
+    getCookingStyles();
+})
+function renderHome() {
+    let mainContent = document.getElementById("main-content")
+    document.getElementById("display-cooking-styles").style.display = "none"
     // debugger
-    let header = document.createElement("h2")
-        header.innerText = e.name
-    document.getElementById("test").appendChild(header)
+    // while (mainContent.hasChildNodes()){
+    //     mainContent.removeChild(mainContent.firstChild)
+    // }
+
+}
+// function getRecipe() {
+//     fetch('http://localhost:3000/recipes/')
+//     .then(res => res.json())
+//     .then(recipes => recipes.forEach(renderRecipe))
+// }
+// function renderRecipe(e) {
+//     // debugger
+//     let header = document.createElement("h2")
+//         header.innerText = e.name
+//     document.getElementById("test").appendChild(header)
+// }
+function getCookingStyles() {
+    if (document.getElementById("display-cooking-styles").hasChildNodes()){
+        document.getElementById("display-cooking-styles").style.display = "block"
+    }
+    else {
+        fetch('http://localhost:3000/recipes/')
+        .then(res => res.json())
+        .then(recipes => recipes.forEach(renderCookingStyle))
+    }
+}
+function renderCookingStyle(e) {
+        let style = document.createElement('h3')
+            style.innerText = e.cuisine
+        document.getElementById("display-cooking-styles").appendChild(style)
 }
