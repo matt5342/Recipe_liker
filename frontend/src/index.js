4aef1cab7cb261a9f61d862dd5446ba3efaff432
Matt <mattfsewell@gmail.com>
2021-03-09 19:59:36 -0500

    search connected to api

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -77,6 +77,7 @@ function resetMainContent() {
 document.getElementsByClassName('form-inline my-2 my-lg-0')[0].addEventListener('submit', handleSearch)
 function handleSearch(e) {
     e.preventDefault()
+    // debugger
     resetMainContent();
     while (document.getElementById("display-from-search").hasChildNodes()) {
         document.getElementById("display-from-search").removeChild(document.getElementById("display-from-search").firstChild)
@@ -84,9 +85,7 @@ function handleSearch(e) {
     let searchTerm = e.target[0].value
     fetch('http://localhost:3000/recipes/search/' + searchTerm)
     .then(res => res.json())
-    .then(recipes => () => {
-        debugger
-        recipes.forEach(renderRecipe)})
+    .then(recipes => recipes.forEach(renderRecipe))
 }
 
 
